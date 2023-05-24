ebcc37029e1b70bcaf6cdaac29196b8aadf47a05
Vipin kumar <vipinkumarvipinkumar20@gmail.com>
2023-05-24 13:48:53 +0530

    Changed default controller

--- TouristWorld/Startup.cs
+++ TouristWorld/Startup.cs
@@ -50,7 +50,7 @@ namespace TouristWorld
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=TouristLocations}/{action=Index}/{id?}");
             });
         }
     }
